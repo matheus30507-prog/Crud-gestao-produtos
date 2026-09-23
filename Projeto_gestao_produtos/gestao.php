786e096b6875691888e54406227a9452ed44c1d6
Matheus <matheus30507@edu.unipar.br>
2026-09-23 15:54:21 -0300

    Versao [2.0] Interface do sistema

--- Projeto_gestao_produtos/gestao.php
+++ Projeto_gestao_produtos/gestao.php
@@ -26,6 +26,52 @@ if (!isset($_SESSION["usuario_id"])) {
             </div>
         </div>
     </nav>
+    <div class="container my-4">
+        
+        <ul class="nav nav-tabs fw-bold" id="sistemaTabs" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="cadastros-tab" data-bs-toggle="tab" data-bs-target="#cadastros" type="button" role="tab">
+                    1. Cadastros
+                </button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="ajax-tab" data-bs-toggle="tab" data-bs-target="#ajax" type="button" role="tab">
+                    2. Área AJAX
+                </button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="selecao-tab" data-bs-toggle="tab" data-bs-target="#selecao" type="button" role="tab">
+                    3. Seleção
+                </button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="cesta-tab" data-bs-toggle="tab" data-bs-target="#cesta" type="button" role="tab">
+                    4. Cesta de Compras
+                </button>
+            </li>
+        </ul>
 
+        <div class="tab-content bg-white p-4 border border-top-0 rounded-bottom shadow-sm">
+            
+            <div class="tab-pane fade show active" id="cadastros" role="tabpanel">
+                <h4 class="mb-3">Área de Cadastros</h4>
+            </div>
+
+            <div class="tab-pane fade" id="ajax" role="tabpanel">
+                <h4 class="mb-3">Área AJAX</h4>
+            </div>
+
+            <div class="tab-pane fade" id="selecao" role="tabpanel">
+                <h4 class="mb-3">Seleção de Produtos</h4>
+            </div>
+
+            <div class="tab-pane fade" id="cesta" role="tabpanel">
+                <h4 class="mb-3">Resumo da Cesta</h4>
+            </div>
+
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
